1b8b6b7be36cb9bdde288151683ea9376ca7d87b
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2021-01-03 14:40:53 +0100

    Update maintenance.sh

--- maintenance/maintenance.sh
+++ maintenance/maintenance.sh
@@ -2,7 +2,8 @@
 
 # update mirrorlist
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-sudo curl -s "https://www.archlinux.org/mirrorlist/?country=DE&protocol=http&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" | sudo sed -e "s/^#Server/Server/g" > sudo /etc/pacman.d/mirrorlist
+sudo pacman -S reflector --noconfirm --needed
+sudo reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 
 # update system
 sudo pacman -Syu
