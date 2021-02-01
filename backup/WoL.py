aecd0966b9503fd1364a9b0af91748c726cca076
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2021-02-01 15:11:56 +0100

    Create WoL.py

---
+++ backup/WoL.py
@@ -0,0 +1,47 @@
+#!/bin/python
+
+import os
+import time
+import configparser
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+class WoL:
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('/home/jogi/programming/python/fun_projects/config_WoL.ini')
+        self.password = self.config['credentials']['password']
+        self.wake_on_lan()
+
+    def wake_on_lan(self):
+        self.init_webdriver()
+        self.login_to_fritzbox_web_ui()
+        self.navigate_to_target_host()
+        self.wake_host()
+
+    def init_webdriver(self):
+        options = Options()
+        options.add_argument('--headless')
+        self.driver = webdriver.Firefox(options=options)
+        addons = [f'{os.getcwd()}/uBlock0_1.32.5b9.firefox.signed.xpi']
+        for addon in addons:
+            self.driver.install_addon(addon)
+
+    def login_to_fritzbox_web_ui(self):
+        self.driver.get('http://192.168.178.1/')
+        self.driver.find_element_by_id('uiPass').send_keys(self.password)
+        self.driver.find_element_by_id('submitLoginBtn').click()
+
+    def navigate_to_target_host(self):
+        self.driver.find_element_by_id('lan').click()
+        self.driver.find_element_by_id('net').click()
+        time.sleep(15)
+        self.driver.find_element_by_id('edit_landevice258').click()
+
+    def wake_host(self):
+        time.sleep(20)
+        # scroll to the end of the page so the "start host" button is not obscured by "ok"/"cancel" button
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        self.driver.find_element_by_xpath('/html/body/main/div[2]/div/div[9]/div/button').click()
+        print('remote host started...')
