1ff8de04bcac2446222b626a646493286aab9f60
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2021-02-02 21:24:24 +0100

    Update maintenance.sh

--- maintenance/maintenance.sh
+++ maintenance/maintenance.sh
@@ -8,9 +8,10 @@ sudo reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 # update system
 sudo pacman -Syu
 
-# clean cached pacman and AUR files
+# clean cached pacman (delete all uninstalled cached packages and only 2 older versions of installed packages)
 sudo pacman -S pacman-contrib --noconfirm --needed
-sudo paccache -r
+sudo paccache -ruk0
+sudo paccache -rk2
 sudo pacman -Rns $(pacman -Qtdq)
 
 # clean chached files and logs
