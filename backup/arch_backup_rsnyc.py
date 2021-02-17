70cd3187b4b91517de25ec9274eaf8f65c63a52b
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2021-02-17 10:30:54 +0100

    Update and rename arch_backup_nfs.py to arch_backup_rsnyc.py

--- backup/arch_backup_rsnyc.py
+++ backup/arch_backup_rsnyc.py
@@ -4,16 +4,18 @@ import os
 import time
 import datetime
 import configparser
+from termcolor import cprint
 from WoL import WoL
 
 
-class Arch_Backup:
-    def __init__(self, base_command, include_list, exclude_list, source, destination, server):
+class ArchBackupRsync:
+    def __init__(self, base_command, include_list, exclude_list, source, destination, username, server):
         self.base_command = base_command
         self.include_list = include_list
         self.exclude_list = exclude_list
         self.source = source
         self.destination = destination
+        self.username = username
         self.server = server
         if not self.remote_host_up():
             # wake remote server and wait for 2 minutes while server is booting
@@ -22,7 +24,7 @@ class Arch_Backup:
         self.start_time = datetime.datetime.now()
         self.backup()
         self.print_runtime()
-        self.shutdown_remote_server()
+        # self.shutdown_remote_server()
 
     def backup(self):
         """
@@ -35,8 +37,8 @@ class Arch_Backup:
             full_command += " " + "--include='" + str(include + "'")
         for exclude in self.exclude_list:
             full_command += " " + "--exclude='" + str(exclude) + "'"
-        full_command += ' ' + str(self.source) + ' ' + str(self.destination)
-        print(full_command)
+        full_command += ' ' + str(self.source) + ' ' + self.username + '@' + self.server + ':' + str(self.destination)
+        cprint(full_command, 'green')
         os.system(full_command)
 
     def remote_host_up(self):
@@ -56,23 +58,22 @@ class Arch_Backup:
 
         end_time = datetime.datetime.now()
         runtime = end_time - self.start_time
-        print('Backup took ' + str(runtime))
+        cprint('Backup took ' + str(runtime), 'green')
 
     def shutdown_remote_server(self):
         os.system(f'ssh root@{self.server} "poweroff"')
 
 
-# load personal information from config file, named config_nfs.ini
+# load personal information from config file, named config_rsync.ini
 config = configparser.ConfigParser()
-config.read('/home/jogi/programming/python/fun_projects/config_nfs.ini')
+config.read('/home/jogi/programming/python/fun_projects/config_rsync.ini')
 base_cmd = 'rsync -av --delete --stats'
 excl_lst = config['options']['exclude_list'].split(' ')
 incl_lst = config['options']['include_list'].split(' ')
 backup_source = config['paths']['backup_source']
 backup_destination = config['paths']['backup_destination']
-nfs_server = config['paths']['nfs_server']
-
+rsync_server = config['paths']['nfs_server']
+ssh_username = config['paths']['username']
 
 if __name__ == '__main__':
-    backup = Arch_Backup(base_cmd, incl_lst, excl_lst, backup_source, backup_destination, nfs_server)
-
+    backup = ArchBackupRsync(base_cmd, incl_lst, excl_lst, backup_source, backup_destination, ssh_username, rsync_server)
