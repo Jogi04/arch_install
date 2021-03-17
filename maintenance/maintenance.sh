1f284c11321e15295df7fdf41a7e4503c8f01659
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2021-03-17 14:00:58 +0100

    Update maintenance.sh

--- maintenance/maintenance.sh
+++ maintenance/maintenance.sh
@@ -6,13 +6,13 @@ sudo pacman -S reflector --noconfirm --needed
 sudo reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 
 # update system
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 
 # clean cached pacman (delete all uninstalled cached packages and only 2 older versions of installed packages)
 sudo pacman -S pacman-contrib --noconfirm --needed
 sudo paccache -ruk0
 sudo paccache -rk2
-sudo pacman -Rns $(pacman -Qtdq)
+sudo pacman -Rns $(pacman -Qtdq) --noconfirm
 
 # clean chached files and logs
 sudo rm -rf .cache/*
