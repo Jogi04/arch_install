400fc67e08fc8a557f470ee87aba43538d8f2323
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2021-04-16 10:37:57 +0200

    Add files via upload

---
+++ backup/arch_restore_rsync.py
@@ -0,0 +1,29 @@
+import os
+import termcolor
+import configparser
+
+
+class ArchRestore:
+    def __init__(self, remote_user, remote_host, backup_directory, restore_destination_directory):
+        self.remote_user = remote_user
+        self.remote_host = remote_host
+        self.backup_directory = backup_directory
+        self.restore_destination_directory = restore_destination_directory
+        self.restore()
+
+    def restore(self):
+        command = f'rsync -av --stats {self.remote_user}@{self.remote_host}:{self.backup_directory} {self.restore_destination_directory}'
+        termcolor.cprint(command, 'green')
+        os.system(command)
+
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('/home/jogi/programming/python/fun_projects/config_rsync_restore.ini')
+    ssh_username = config['paths']['username']
+    rsync_server = config['paths']['rsync_server']
+    backup_dir = config['paths']['backup_dir']
+    restore_destination = config['paths']['restore_destination']
+
+    test = ArchRestore(remote_user=ssh_username, remote_host=rsync_server, backup_directory=backup_dir,
+                       restore_destination_directory=restore_destination)
